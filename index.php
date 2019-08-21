d557fa37c796bb67f57b59cb195bcd1d5d36b207
atomickarma <54375176+atomickarma@users.noreply.github.com>
2019-08-21 20:17:02 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php echo file_get_contents("html/header.html"); ?>
+<?php echo file_get_contents("html/body.html"); ?>
+<?php echo file_get_contents("html/footer.html"); ?>
+<p>Copyright © CM Cawley <?php echo date("Y"); ?></p> 
